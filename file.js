c8cf2c34315ce1dee09a10b75a68df3099927e61
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-02-14 23:52:56 +0530

    learning howw to console and print

---
+++ file.js
@@ -0,0 +1,131 @@
+
+//Objects//
+let students=[ {
+        name: "Om Darade",
+        roll: 13,
+        age: 24
+    },
+    {
+        name: "Vrushali Darade",
+        roll: 01,
+        age: 23
+    },
+    {
+        name: "Krushna Darade",
+        roll: 21,
+        age: 17
+    },
+    {
+        name: "Pooja Darade",
+        roll: 15,
+        age: 19
+    },
+    {
+        name: "Shubhangi Darade",
+        roll: 09,
+        age: 47
+    },
+    {
+        name: "Sanjay darade",
+        roll: 16,
+        age: 48
+    },
+    {
+        name: "Ishwari Gite",
+        roll: 07,
+        age: 15
+    },
+    {
+        name: "Sunita Tathe",
+        roll: 05,
+        age: 42
+    },
+    {
+        name: "Ramdas Tathe",
+        roll: 25,
+        age: 45
+    },
+    {
+        name: "Aditya Mundhe",
+        roll: 19,
+        age: 20
+    }
+];
+
+console.log("name:"+" "+students[0].name);
+console.log("roll:"+" "+students[0].roll);
+console.log("age:"+" "+students[0].age);
+console.log("name:"+" "+students[1].name);
+console.log("roll:"+" "+students[1].roll);
+console.log("age:"+" "+students[1].age);
+console.log("name:"+" "+students[2].name);
+console.log("roll:"+" "+students[2].roll);
+console.log("age:"+" "+students[2].age);
+console.log("name:"+" "+students[3].name);
+console.log("roll:"+" "+students[3].roll);
+console.log("age:"+" "+students[3].age);
+console.log("name:"+" "+students[4].name);
+console.log("roll:"+" "+students[4].roll);
+console.log("age:"+" "+students[4].age);
+console.log("name:"+" "+students[5].name);
+console.log("roll:"+" "+students[5].roll);
+console.log("age:"+" "+students[5].age);
+console.log("name:"+" "+students[6].name);
+console.log("roll:"+" "+students[6].roll);
+console.log("age:"+" "+students[6].age);
+console.log("name:"+" "+students[7].name);
+console.log("roll:"+" "+students[7].roll);
+console.log("age:"+" "+students[7].age);
+console.log("name:"+" "+students[8].name);
+console.log("roll:"+" "+students[8].roll);
+console.log("age:"+" "+students[8].age);
+console.log("name:"+" "+students[9].name);
+console.log("roll:"+" "+students[9].roll);
+console.log("age:"+" "+students[9].age);
+
+//Arrays//
+let cars=["maruti","lamborgini","mercedez"];
+let bikes=["suzuki","splender","bajaj"];
+let fruits=["mango","apple","banana"];
+let vegetables=["cabbage","brinjal","cauliflower"];
+let person=["om","pooja","krushna"];
+let dogs=["labrador","pug","german shepherd"];
+let cities=["sambhajinagar","pune","mumbai"];
+let states=["maharashtra","gujarat","rajasthan"];
+let countries=["bharat","israel","japan"];
+let continent=["asia","america","australia"];
+
+console.log("Type of a car:"+" "+cars[0]);
+console.log("Type of a car:"+" "+cars[1]);
+console.log("Type of a car:"+" "+cars[2]);
+console.log("Type of a bike:"+" "+bikes[0]);
+console.log("Type of a bike:"+" "+bikes[1]);
+console.log("Type of a bike:"+" "+bikes[2]);
+console.log("Type of a fruit:"+" "+fruits[0]);
+console.log("Type of a fruit:"+" "+fruits[1]);
+console.log("Type of a fruit:"+" "+fruits[2]);
+console.log("Type of a vegetable:"+" "+vegetables[0]);
+console.log("Type of a vegetable:"+" "+vegetables[1]);
+console.log("Type of a vegetable:"+" "+vegetables[2]);
+console.log("Type of a person:"+" "+person[0]);
+console.log("Type of a person:"+" "+person[1]);
+console.log("Type of a person;"+" "+person[2]);
+console.log("Type of a dog:"+" "+dogs[0]);
+console.log("Type of a dog:"+" "+dogs[1]);
+console.log("Type of a dog:"+" "+dogs[2]);
+console.log("Type of a city:"+" "+cities[0]);
+console.log("Type of a city:"+" "+cities[1]);
+console.log("Type of a city:"+" "+cities[2]);
+console.log("Type of a state:"+" "+states[0]);
+console.log("Type of a state:"+" "+states[1]);
+console.log("Type of a state:"+" "+states[2]);
+console.log("Type of a country:"+" "+countries[0]);
+console.log("Type of a country:"+" "+countries[1]);
+console.log("Type of a country:"+" "+countries[2]);
+console.log("Type of a continent:"+" "+continent[0]);
+console.log("Type of a continent:"+" "+continent[1]);
+console.log("Type of a continent:"+" "+continent[2]);
+
+
+//Variables//
+
