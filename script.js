840caf75abd698cc256d0f9dc3156e0a5489ff8e
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-04-05 17:30:07 +0530

    created buttons for single and double clicks

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-//? Practice 1//
 
-//  let newBtn = document.createElement("button");
-//  newBtn.innerText = "Click me!";
-//  newBtn.style.backgroundColor = "red";
-//  newBtn.style.color = "white";
-//  document.querySelector("body").prepend(newBtn);
-
-//? Practice 2//
-
-let para = document.querySelector("p");
-para.classList.add("newClass");
 
 
 
