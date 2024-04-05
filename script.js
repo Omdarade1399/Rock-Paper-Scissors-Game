ce429fefaa5e0f218d88cae348bbc096cd348c58
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-04-05 17:16:14 +0530

    created a new class in a para using classlist  with javascript

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-//* Practice 1//
+//? Practice 1//
+
+//  let newBtn = document.createElement("button");
+//  newBtn.innerText = "Click me!";
+//  newBtn.style.backgroundColor = "red";
+//  newBtn.style.color = "white";
+//  document.querySelector("body").prepend(newBtn);
+
+//? Practice 2//
+
+let para = document.querySelector("p");
+para.classList.add("newClass");
 
 
-let newBtn = document.createElement("button");
-newBtn.innerText = "Click me!";
-newBtn.style.backgroundColor = "red";
-newBtn.style.color = "white";
-document.querySelector("body").prepend(newBtn);
+
+
+
+
+
 
 
