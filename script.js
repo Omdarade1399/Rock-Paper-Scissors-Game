d0dca204c29e5d3085c61e647bf4c76aff046a46
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-04-06 14:21:44 +0530

    using a simpler way to change modes

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let modeBtn = document.querySelector("#mode");
 let currMode = "light";
-modeBtn = addEventListener( "click", () => {
-      if(currMode === "light"){
-        currMode = "dark";
-        document.querySelector("body").style.backgroundColor = "black";
-      }else{
-        currMode = "light";
-        document.querySelector("body").style.backgroundColor = "white";
-      }
-      console.log(currMode);
-});
+let changeMode = () => {
+    if(currMode === "light"){
+      currMode = "dark";
+      document.querySelector("body").style.backgroundColor = "black";
+    }else{
+      currMode = "light";
+      document.querySelector("body").style.backgroundColor = "white";
+    }
+    console.log(currMode);
+}
+modeBtn = addEventListener( "click", changeMode );
 
 
 
