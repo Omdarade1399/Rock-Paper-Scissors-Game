277cd83c9b720c1234076973ffc18568249097c9
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-04-06 16:39:02 +0530

    almost done making the game

--- script.js
+++ script.js
@@ -9,11 +9,37 @@ const genCompChoice = () => {
     return options[randIdx];
 }
 
+const drawGame = () => {
+    console.log("Game is draw");
+}
+
+const showWinner = (userWin) => {
+    if(userWin){
+        console.log("You win the round!");
+    }else{
+        console.log("You lose the round!");
+    }
+}
+
 const playGame = (userChoice) => {
       console.log("users chose = ", userChoice);
       
       const compChoice = genCompChoice();
       console.log("computer chose = ", compChoice);
+
+      if(userChoice === compChoice){
+          drawGame();
+      }else {
+        let userWin = true;
+        if(userChoice === "rock"){
+            userWin = compChoice === "paper" ? false : true;
+        }else if(userChoice === "paper"){
+            userWin = compChoice === "scissor" ? false :true;
+        }else{
+            userWin = compChoice === "rock" ? false : true; 
+        }
+        showWinner(userWin);
+      }
 }
 
  choices.forEach((choice) => {
