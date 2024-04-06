96e7d6e3f8e2823112341ce287ba1cc9ab1fd594
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-04-06 16:01:32 +0530

    making a rock paper scissors game

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-let modeBtn = document.querySelector("#mode");
-let currMode = "light";
-let changeMode = () => {
-    if(currMode === "light"){
-      currMode = "dark";
-      document.querySelector("body").style.backgroundColor = "black";
-    }else{
-      currMode = "light";
-      document.querySelector("body").style.backgroundColor = "white";
-    }
-    console.log(currMode);
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissor"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
 }
-modeBtn = addEventListener( "click", changeMode );
+
+const playGame = (userChoice) => {
+      console.log("users chose = ", userChoice);
+      
+      const compChoice = genCompChoice();
+      console.log("computer chose = ", compChoice);
+}
+
+ choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+ });
+ });
 
 
 
