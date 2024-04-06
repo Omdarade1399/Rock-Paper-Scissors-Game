a15afed6998a71471d68e682bd2b769bc227dd00
Omdarade1399 <150209165+Omdarade1399@users.noreply.github.com>
2024-04-06 14:13:23 +0530

    created an event to change light mode to dark and so onn

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-
+let modeBtn = document.querySelector("#mode");
+let currMode = "light";
+modeBtn = addEventListener( "click", () => {
+      if(currMode === "light"){
+        currMode = "dark";
+        document.querySelector("body").style.backgroundColor = "black";
+      }else{
+        currMode = "light";
+        document.querySelector("body").style.backgroundColor = "white";
+      }
+      console.log(currMode);
+});
 
 
 
